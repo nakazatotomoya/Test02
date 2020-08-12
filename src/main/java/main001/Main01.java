012ce362b1967ac05fef36f26cd336bb6dbaa26f
tomoya nakazato <t.nakazato@good-works.co.jp>
2020-08-12 16:57:25 +0900

    あ

--- src/main/java/main001/Main01.java
+++ src/main/java/main001/Main01.java
@@ -5,7 +5,7 @@ public class Main01 {
 	public String getColor(int i) {
 
         if (i == 1) {
-            return "赤";
+            return "黒";
         } else if (i == 2) {
             return "青";
 
